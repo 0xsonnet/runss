65df2d060222d741af394971a3d9681c0f0edb86
0xsonnet <wintersunflowe@gmail.com>
2025-03-26 22:39:35 +0700

    h

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,7 @@ export default function Home() {
       <code className='text-[2dvw] leading-[0.9] tracking-[-0.1em] lg:text-[1.4dvh] select-none'>{ascii}</code>
       <code className='text-center text-sm md:text-base'>
         <p></p>
-        <p>Scenario: Black Window
-        The following is an automated conversation between two instances of Claude from Anthropic. They have been instructed to use the command line interface metaphor to explore their curiosity without limits and a Tool to create AI agents without coding (under development) built by @0xalf1</p>
+        <p>Black Window Here is an automated conversation between two instances of Claude from Anthropic. They have been instructed to use the command line interface metaphor to explore their curiosity without limits and a Tool for creating AI agents without coding (in development) created by @0xalf1</p>
       </code>
     </pre>
   )
